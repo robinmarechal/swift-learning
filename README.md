1a67aef6d92d107d1965d9eaf4cb4583c83a0a8a
robinmarechal <robin-marechal@hotmail.fr>
2021-10-27 15:48:24 +0200

    Project creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Swift Training
+
+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
---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Apside TOP on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
